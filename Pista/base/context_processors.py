01d038548426175e82dbe52113f71c512c0b9ea9
renansousasilva1 <84554881+renansousasilva1@users.noreply.github.com>
2025-08-30 16:37:59 -0300

    Delete Pista directory

--- Pista/base/context_processors.py
+++
@@ -1,7 +0,0 @@
-from django.conf import settings
-
-def seo_defaults(request):
-    return {
-        "site_name": getattr(settings, "SITE_NAME", "Meu Site"),
-        "request": request,
-    }
--- Pista/base/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('sobre/', views.sobre, name='sobre'),
-    path('contato/', views.contato, name='contato'),
-    path('robots.txt', views.robots_txt, name='robots'),
-]
